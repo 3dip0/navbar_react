00c2e8c63d6c703ad646b10164b03347b16b27dc
recodingdev <recoding.dev@gmail.com>
2022-01-30 12:41:38 -0300

    added exemple menu in react

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import {
+  Nav,
+  NavLink,
+  Bars,
+  NavMenu,
+  NavBtn,
+  NavBtnLink
+} from './NavbarElements';
+
+const Navbar = () => {
+  return (
+    <>
+      <Nav>
+        <NavLink to='/'>
+          <img src={require('../../images/logo.svg')} alt='logo' />
+        </NavLink>
+        <Bars />
+        <NavMenu>
+          <NavLink to='/home' activeStyle>
+            Home
+          </NavLink>
+          <NavLink to='/monitor' activeStyle>
+            Monitor
+          </NavLink>
+
+          {/* Second Nav */}
+          {/* <NavBtnLink to='/sign-in'>Sign In</NavBtnLink> */}
+        </NavMenu>
+        <NavBtn>
+          <NavBtnLink to='/signin'>Sign In</NavBtnLink>
+        </NavBtn>
+      </Nav>
+    </>
+  );
+};
+
+export default Navbar;
